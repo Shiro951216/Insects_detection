f9957732dfe4ed4e490dc0955fbb4d9763114d49
Shiro951216 <yoshiro.vilchez@unmsm.edu.pe>
2023-06-09 02:03:50 -0500

    git push origin master

--- predict.py
+++ predict.py
@@ -11,6 +11,7 @@ model_path = os.path.join('.', 'runs', 'detect', 'train', 'weights', 'best.pt')
 threshold = 0.5
 
 class_name_dict = {1: 'insect'}
+directory = r'C:\Users\Yoshiro\Desktop\insect_detection\images'
 
 model = YOLO(model_path)
 result = model(img_path)[0]
@@ -20,6 +21,8 @@ for result in result.boxes.data.tolist():
         cv2.rectangle(img, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 0), 4)
         cv2.putText(img, class_name_dict[int(class_id)].upper(), (int(x1), int(y1 - 10)),
                     cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 255, 0), 3, cv2.LINE_AA)
+os.chdir(directory)
+cv2.imwrite("predicted.jpg", img)
 cv2.imshow("img", img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
