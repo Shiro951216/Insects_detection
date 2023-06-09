28a3d688429cf87e221b8ec24ecee310835e5958
Shiro951216 <yoshiro.vilchez@unmsm.edu.pe>
2023-06-09 01:50:52 -0500

    first commit

---
+++ data/README.dataset.txt
@@ -0,0 +1,6 @@
+# FAIR-D_V3 > 2023-01-26 4:06pm
+https://universe.roboflow.com/juan-chiavassa/fair-d_v3
+
+Provided by a Roboflow user
+License: Public Domain
+
---
+++ predict.py
@@ -0,0 +1,25 @@
+import os
+from ultralytics import YOLO
+import cv2
+
+IMG_DIR = os.path.join('.', 'images')
+
+img_path = os.path.join(IMG_DIR, 'img1.jpg')
+img = cv2.imread(img_path)
+model_path = os.path.join('.', 'runs', 'detect', 'train', 'weights', 'best.pt')
+
+threshold = 0.5
+
+class_name_dict = {1: 'insect'}
+
+model = YOLO(model_path)
+result = model(img_path)[0]
+for result in result.boxes.data.tolist():
+    x1, y1, x2, y2, score, class_id = result
+    if score > threshold:
+        cv2.rectangle(img, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 0), 4)
+        cv2.putText(img, class_name_dict[int(class_id)].upper(), (int(x1), int(y1 - 10)),
+                    cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 255, 0), 3, cv2.LINE_AA)
+cv2.imshow("img", img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
